5e69176103434c7736e337e8f380c30d2651f2c0
Ritorta <Meddesu@yandex.ru>
2023-04-11 10:26:01 +0500

    W1 Done

---
+++ Work№1/Task1/W1Z1.py
@@ -0,0 +1,56 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+
+# *Пример:*
+
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0) 
+
+# ------------
+# Вариант №1
+# ------------
+
+# n = input('Enter three-digit number: ')
+# count = len(n)
+# if count == 3:
+#     n = int(n)
+#     n1 = n % 10
+#     n2 = n % 100 // 10
+#     n3 = n // 100
+#     sum = n1 + n2 + n3
+#     print('Sum numbers', sum)
+# else:
+#     print('Error, enter the three-digit.')
+
+# ------------
+# Вариант №2
+# ------------
+
+# n = input('Enter three-digit number: ')
+
+# count = len(n)
+# if count == 3:
+#     n1 = int(n[0])
+#     n2 = int(n[1])
+#     n3 = int(n[2])
+#     sum = n1 + n2 + n3
+#     print('Sum numbers:',sum)
+# else:
+#     print('Error, enter the three-digit.')
+
+# ------------
+# Вариант №3
+# ------------
+
+n = input('Enter three-digit number: ')
+
+count = len(n)
+if count == 3:
+    n = int(n)
+    d1 = n % 10
+    n1 = n // 10
+    d2 = n1 % 10
+    n2 = n1 // 10
+    sum = n2 + d1 + d2
+    print(sum)
+else:
+    print('error')    
---
+++ Work№1/Task3/W1Z3.py
@@ -0,0 +1,54 @@
+# Задача 6: Вы пользуетесь общественным транспортом? 
+# Вероятно, вы расплачивались за проезд и получали билет с номером. 
+# Счастливым билетом называют такой билет с шестизначным номером, где сумма первых трех цифр равна сумме последних трех. 
+# Т.е. билет с номером 385916 – счастливый, т.к. 3+8+5=9+1+6. Вам требуется написать программу, которая проверяет счастливость билета.
+
+# *Пример:*
+
+# 385916 -> yes
+# 123456 -> no
+
+# ------------
+# Вариант №1
+# ------------
+
+# n = input('Enter six-digit number ticket: ')
+
+# list1 = int(n[0]) + int(n[1]) + int(n[2])
+# list2 = int(n[3]) + int(n[4]) + int(n[5])
+# count = len(n)
+# if count == 6:
+#     if list1 == list2:
+#         print('Yes, thes lucky ticket')
+#     else:
+#         print('Oh no, thes ticket losers')
+# else:
+#     print('Error, enter six-digit number!')
+
+# ------------
+# Вариант №2
+# ------------
+
+n = input('Enter six-digit number tiket: ')
+
+firstn = int(0)
+secondn = int(0)
+count = len(n)
+
+if count == 6:
+    n = int(n)
+    for i in range(6):
+        if i < 3:
+            firstn = firstn + n // 10 ** i % 10
+        else:
+            secondn = secondn + n // 10 ** i % 10
+    if firstn == secondn:
+        print('Yes, thes lucky ticket')         
+    else:
+        print('Oh no, thes ticket losers')
+else:
+    print('Error, enter six-digit number!')
+
+
+
+
