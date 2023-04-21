baccc055e1f22db940b6e4c417221a230f081831
Ritorta <Meddesu@yandex.ru>
2023-04-21 12:26:48 +0500

    W3T2 Done?

--- Work№3/Task2/W3T2.py
+++ Work№3/Task2/W3T2.py
@@ -9,17 +9,110 @@
 # 6
 # -> 5
 
-n = int(input('Enter size array: '))
+# ------------
+# Вариант №1
+# ------------
+
+# n = abs(int(input('Enter the size array: ')))
+
+# a = []
+
+# for i in range(n):
+#     a.append(abs(int(input('Enter the number: '))))
+
+# x = abs(int(input('Enter the key number: ')))
+
+# dif = abs(a[0] - x)
+# result = a[0]
+
+# for i in a:
+#     if abs(i - x) < dif:
+#         dif = abs(i - x)
+#         result = i
+
+# print(result)
+
+# ------------
+# Вариант №2
+# ------------
+
+# n = abs(int(input('Enter size array: ')))
+
+# a =[]
+
+# for i in range(n):
+#     a.append(abs(int(input('Enter the number: '))))
+
+# x = abs(int(input('Enter the key number: ')))
+
+# result_min = x - 1
+# result_max = result_min + 2
+
+# print(result_min)
+# print(result_max)
+
+# ------------
+# Вариант №3
+# ------------
+
+# n = abs(int(input('Enter size array: ')))
+
+# a =[]
+
+# for i in range(n):
+#     a.append(abs(int(input('Enter the number: '))))
+
+# x = abs(int(input('Enter the key number: ')))
+
+# dif_min = abs(a[0] - x)
+
+# result_min = a[0]
+# result_max = a[-1]
+
+# for i in a:
+#     if abs(i - x) < dif_min:
+#         dif_min = abs(i - x)
+#         result_min = i
+#     dif_max = abs(result_min + 2)
+#     result_max = dif_max
+
+#     if i == x:
+#         result_min = result_min - 1
+#         result_max = result_min + 2
+                
+# print(result_min)
+# print(result_max)
+
+
+n = abs(int(input('Enter size array: ')))
 
 a =[]
 
 for i in range(n):
-    a.append(int(input('Enter the number: ')))
+    a.append(abs(int(input('Enter the number: '))))
 
-x = int(input('Enter the key number: '))
+x = abs(int(input('Enter the key number: ')))
 
-for i in range(n):
-    if x >= n:
-        print(i)
+dif_min = abs(a[0] - x)
 
-print(a)
+result_min = a[0]
+result_max = a[-1]
+
+
+for i in a:
+    if abs(i - x) < dif_min:
+        dif_min = abs(i - x)
+        result_min = i
+        dif_max = abs((result_max - result_min) * 2)
+        result_max = dif_max
+
+    if i == x:
+        result_min = result_min - 1
+        result_max = result_min + 2
+
+
+
+
+                
+print(result_min)
+print(result_max)
