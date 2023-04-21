5180a00fd1b6fc262fe8fcf233d1948bee16ea79
Ritorta <Meddesu@yandex.ru>
2023-04-21 13:41:59 +0500

    W3T3 Done?

--- Work№3/Task3/W3T3.py
+++ Work№3/Task3/W3T3.py
@@ -24,3 +24,55 @@
 # Вывод:
 # 12
 
+# ------------
+# Вариант №1
+# ------------
+
+list_ru = {1 : 'АВЕИНОРСТ',   
+           2 : 'ДКЛМПУ',
+           3 : 'БГЁЬЯ',
+           4 : 'ЙЫ',
+           5 : 'ЖЗХЦЧ',
+           8 : 'ШЭЮ',
+           10 : 'ФЩЪ'}
+
+list_en = {1 : 'AEIOULNSTR',
+           2 : 'DG',
+           3 : 'BCMP',
+           4 : 'FHVWY',
+           5 : 'K',
+           8 : 'JX',
+           10 : 'QZ'}
+
+select = str(input('Select language, enter the "RUS" or "ENG": ')).upper()
+
+if select == 'ENG':
+
+    word = str(input('Enter the word: ')).upper()
+
+    if word.isalpha():
+        sum_en = 0
+        for i in list_en:
+            for j in word:
+                if j in list_en[i]:
+                    sum_en += i
+        print(sum_en)
+    else:
+        print('Error, please enter the word.')      
+
+elif select == 'RUS':
+
+    word = str(input('Enter the word: ')).upper()
+    
+    if word.isalpha():
+        sum_ru = 0
+        for i in word:
+            for j in list_ru:
+                if i in list_ru[j]:
+                    sum_ru += j
+        print(sum_ru)
+    else:
+        print('Error, please enter the word.')    
+
+else:
+    print('Error, incorect command, please enter the "RUS" or "ENG".')
