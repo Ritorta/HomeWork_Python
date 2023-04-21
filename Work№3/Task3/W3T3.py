dac0930217582349bdb3310c4c621444b79f34ab
Ritorta <Meddesu@yandex.ru>
2023-04-21 14:34:57 +0500

    W3T3 Done

--- Work№3/Task3/W3T3.py
+++ Work№3/Task3/W3T3.py
@@ -30,51 +30,83 @@
 # Вариант №1
 # ------------
 
-list_ru = {1 : 'АВЕИНОРСТ',   
-           2 : 'ДКЛМПУ',
-           3 : 'БГЁЬЯ',
-           4 : 'ЙЫ',
-           5 : 'ЖЗХЦЧ',
-           8 : 'ШЭЮ',
-           10 : 'ФЩЪ'}
+# list_ru = {1 : 'АВЕИНОРСТ',   
+#            2 : 'ДКЛМПУ',
+#            3 : 'БГЁЬЯ',
+#            4 : 'ЙЫ',
+#            5 : 'ЖЗХЦЧ',
+#            8 : 'ШЭЮ',
+#            10 : 'ФЩЪ'}
 
-list_en = {1 : 'AEIOULNSTR',
-           2 : 'DG',
-           3 : 'BCMP',
-           4 : 'FHVWY',
-           5 : 'K',
-           8 : 'JX',
-           10 : 'QZ'}
+# list_en = {1 : 'AEIOULNSTR',
+#            2 : 'DG',
+#            3 : 'BCMP',
+#            4 : 'FHVWY',
+#            5 : 'K',
+#            8 : 'JX',
+#            10 : 'QZ'}
 
-select = str(input('Select language, enter the "RUS" or "ENG": ')).upper()
+# select = str(input('Select language, enter the "RUS" or "ENG": ')).upper()
 
-if select == 'ENG':
+# if select == 'ENG':
 
-    word = str(input('Enter the word: ')).upper()
+#     word = str(input('Enter the word: ')).upper()
 
-    if word.isalpha():
-        sum_en = 0
-        for i in list_en:
-            for j in word:
-                if j in list_en[i]:
-                    sum_en += i
-        print(sum_en)
-    else:
-        print('Error, please enter the word.')      
+#     if word.isalpha():
+#         sum_en = 0
+#         for i in list_en:
+#             for j in word:
+#                 if j in list_en[i]:
+#                     sum_en += i
+#         print(sum_en)
+#     else:
+#         print('Error, please enter the word.')      
 
-elif select == 'RUS':
+# elif select == 'RUS':
 
-    word = str(input('Enter the word: ')).upper()
+#     word = str(input('Enter the word: ')).upper()
     
-    if word.isalpha():
-        sum_ru = 0
-        for i in word:
-            for j in list_ru:
-                if i in list_ru[j]:
-                    sum_ru += j
-        print(sum_ru)
-    else:
-        print('Error, please enter the word.')    
+#     if word.isalpha():
+#         sum_ru = 0
+#         for i in word:
+#             for j in list_ru:
+#                 if i in list_ru[j]:
+#                     sum_ru += j
+#         print(sum_ru)
+#     else:
+#         print('Error, please enter the word.')    
+
+# else:
+#     print('Error, incorect command, please enter the "RUS" or "ENG".')
+
+# ------------
+# Вариант №2
+# ------------
+
+book = {'АВЕИНОРСТ' : 1,   
+        'ДКЛМПУ' : 2,
+        'БГЁЬЯ' : 3,
+        'ЙЫ' : 4,
+        'ЖЗХЦЧ' : 5,
+        'ШЭЮ' : 8,
+        'ФЩЪ' : 10,
+        'AEIOULNSTR' : 1,
+        'DG' : 2,
+        'BCMP' : 3,
+        'FHVWY' : 4,
+        'K' : 5,
+        'JX' : 8,
+        'QZ' : 10}
+
+words = str(input('Enter the words, Ru or En language: ')).upper()
+sum = 0
+
+if words.isalpha():
+    for k, v in book.items():
+        for i in words:
+            if i in k:
+                sum += v
+    print(sum)    
 
 else:
-    print('Error, incorect command, please enter the "RUS" or "ENG".')
+    print('Error, please enter the words.')    
