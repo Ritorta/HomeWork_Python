3ff02db7b39e5d6575b2b07c622f030a4bfb025b
Ritorta <Meddesu@yandex.ru>
2023-04-24 14:06:49 +0500

    readme add w5 info

--- Work№5/Task1/W5T1.py
+++ Work№5/Task1/W5T1.py
@@ -3,6 +3,36 @@
 # *Пример:*
 
 # A = 3; B = 5 -> 243 (3⁵)
-#     A = 2; B = 3 -> 8 
+# A = 2; B = 3 -> 8 
+
+# ------------
+# Вариант №1
+# ------------
+
+def degree(num, deg):
+    if deg == 1:
+        return num
+    
+    return degree(num * count, deg - 1)
+    
+num = abs(int(input('a: ')))
+deg = abs(int(input('b: ')))
+count = num
+    
+print(degree(num, deg))
+
+# ------------
+# Вариант №2
+# ------------
 
 
+# def degree(num, deg):
+#     if deg == 1:
+#         return num
+    
+#     return num * degree(num, deg - 1)
+    
+# num = abs(int(input('a: ')))
+# deg = abs(int(input('b: ')))
+    
+# print(degree(num, deg))
