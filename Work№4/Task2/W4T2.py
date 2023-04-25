c4fc180704857f49bcb14cc68a9d9a4309f1331c
Ritorta <Meddesu@yandex.ru>
2023-04-25 15:47:23 +0500

    W4T2 Done

--- Work№4/Task2/W4T2.py
+++ Work№4/Task2/W4T2.py
@@ -19,3 +19,52 @@
 # Вывод:
 # 9
 
+# ------------
+# Вариант №1
+# ------------
+
+# n = abs(int(input('Enter the number of bushes: ')))
+# if n > 2:
+
+#     bushes = []
+#     sum = []
+
+#     for i in range(n):
+#         bushes.append(abs(int(input(f'Bushes number {i + 1}, enter to the number berriers: '))))
+
+#     for j in range(len(bushes) - 1):
+#             sum.append(bushes[j] + bushes[j - 1] + bushes[j + 1])
+#     sum.append(bushes[-1] + bushes[-2] + bushes[0]) 
+    
+#     max_value = max(sum)
+#     max_index = sum.index(max_value) + 1
+
+
+
+#     print('Sum berriers:', sum)
+#     print(f'Maximum number of berries per bush is equal to: {max_value}, maximum berriers in bushes number: {max_index}.')
+# else:
+#     print('Error, need thee an more bushes.')         
+
+
+# ------------
+# Вариант №2
+# ------------
+
+n = abs(int(input('Enter the number of bushes: ')))
+
+bushes = []
+sum = 0
+
+for i in range(n):
+    bushes.append(abs(int(input(f'Bushes number {i + 1}, enter to the number berriers: '))))
+
+bushes += bushes[:2]
+
+for j in range(n):
+    sum = max(sum, bushes[j] + bushes[j + 1] + bushes[j + 2])
+
+
+print('Maximum berries:', sum)
+
+
