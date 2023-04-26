7d1a1cb75496e4e0018075f3258a24faae538ae5
Ritorta <Meddesu@yandex.ru>
2023-04-26 16:03:00 +0500

    Add code

--- Work№6/Task2/W6T2.py
+++ Work№6/Task2/W6T2.py
@@ -13,3 +13,27 @@
 # ------------
 # Вариант №1
 # ------------
+
+m = int(input('Enter the size array: '))
+
+array = []
+
+for i in range(m):
+    array.append(int(input(f'Enter number array {i + 1}: ')))
+
+# interval = int(input('Enter the number size interval: '))
+
+array_i = []
+z = []
+
+for j in range(1, 3):
+    array_i.append(int(input(f'Enter the number interval {j + 1}: ')))
+
+for i1 in range(len(array)):
+    for j1 in array_i:
+        if array_i <= array and array_i >= array:
+            z.append(i1)
+
+
+print(array)
+print(array_i)
