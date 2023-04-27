f6e5414fa1b6ad31c1f6e90e9b250420fa0e37af
Ritorta <Meddesu@yandex.ru>
2023-04-27 10:37:38 +0500

    W6T2 Done

--- Work№6/Task2/W6T2.py
+++ Work№6/Task2/W6T2.py
@@ -21,19 +21,36 @@ array = []
 for i in range(m):
     array.append(int(input(f'Enter number array {i + 1}: ')))
 
-# interval = int(input('Enter the number size interval: '))
-
 array_i = []
-z = []
+index = [] 
 
 for j in range(1, 3):
     array_i.append(int(input(f'Enter the number interval {j + 1}: ')))
 
 for i1 in range(len(array)):
-    for j1 in array_i:
-        if array_i <= array and array_i >= array:
-            z.append(i1)
+        if array_i[0] <= array[i1] <= array_i[1]:
+            index.append(i1)
 
+print(f'Index in interval from {array_i[0]} to {array_i[1]}: {index}')
 
-print(array)
-print(array_i)
+# ------------
+# Вариант №2
+# ------------
+
+# m = int(input('Enter the size array: '))
+
+# array = []
+
+# for i in range(m):
+#     array.append(int(input(f'Enter number array {i + 1}: ')))
+
+# interval_first = int(input('Enter the number size interval: '))
+# interval_second = int(input('Enter the number size interval: '))
+
+# array_i = []
+
+# for i1 in range(len(array)):
+#         if interval_first <= array[i1] <= interval_second:
+#             array_i.append(i1)
+
+# print(f'Index in interval from {interval_first} to {interval_second}: {array_i}')
