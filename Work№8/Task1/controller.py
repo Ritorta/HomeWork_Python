1b41f70d131ff20a1c07c18c1f83d3c91b0567c5
Ritorta <Meddesu@yandex.ru>
2023-06-22 14:20:50 +0500

    Fix Controller

--- Work№8/Task1/controller.py
+++ Work№8/Task1/controller.py
@@ -1,85 +1,37 @@
+import view
+import database
 
-def add_base(dat):
-    with open('BASE.txt', 'a', encoding='utf-8') as file:
-        file.write(dat)
-    print('Запись добавлена.')
-
-def searh_base(dat):
-    with open('BASE.txt', 'r', encoding='utf-8') as file:
-        sear = file.readlines()
-        flag = False
-        for i in sear:
-            if dat in i:
-                flag = True
-                print(i)
-
-        if flag == False:
-            print('Записи не существует.\n')
-    input("Нажмите клавижшу для продолжения: ")
-
-def sort_base_name():
-    with open('BASE.txt', 'r', encoding='utf-8') as file:
-        sorts = file.readlines()
-        sorts.sort()
-    with open('BASE.txt', 'w', encoding='utf-8') as file:
-        file.writelines(sorts)
-        print('Сортировка завершина.')
-    input("Нажмите клавижшу для продолжения: ")
-
-def sort_base_date():
-    with open('BASE.txt', 'r', encoding='utf-8') as file:
-        sorts = file.readlines()
-        sorts.sort(key=lambda x: x[3])
-    with open('BASE.txt', 'w', encoding='utf-8') as file:
-        file.writelines(sorts)
-        print('Сортировка завершина.')
-    # input("Нажмите клавижшу для продолжения: ")
-
-def show_base_name():
-    with open('BASE.txt', 'r', encoding='utf-8') as file:
-        wi = file.readlines()
-        for i in wi:
-            print(i.split(';')[0])
-        input("Нажмите клавижшу для продолжения: ")
-
-def select_number(dat):
-    lst_edit = []
-    count = 1
-    with open('BASE.txt', 'r', encoding='utf-8') as file:
-        edit = file.readlines()
-        for i in edit:
-            if dat in i:
-                print(f"{count}) {i.strip()}")
-                count += 1
-                lst_edit.append(i)
-    return lst_edit
-
-def change_base(old_str, new_str):
-    with open('BASE.txt', 'r', encoding='utf-8') as file:
-        lst_delete = file.readlines()
-    with open('BASE.txt', 'w', encoding='utf-8') as file:
-        for i in lst_delete:
-            if i == old_str:
-                file.write(new_str)
-                continue
-            file.write(i)
-    print('Запись изменена.')
-    input("Нажмите клавижшу для продолжения: ")
-
-def delete_name_base(dat):
-    with open('BASE.txt', 'r', encoding='utf-8') as file:
-        lst_delete = file.readlines()
-    with open('BASE.txt', 'w', encoding='utf-8') as file:
-        for i in lst_delete:
-            if dat == i:
-                continue
-            file.write(i)
-    print('Запись удалена!')
-    input("Нажмите клавижшу для продолжения: ")
+def main():
+    while True:
+        num = view.user_input()
+        if num == 1:
+            dat = view.input_1()
+            database.add_base(dat)
+        elif num == 2:
+            dat = view.input_2()
+            database.searh_base(dat)
+        elif num == 3:
+            database.sort_base_name()
+        elif num == 4:
+            database.sort_base_date()
+        elif num == 5:
+            database.show_base()
+        elif num == 6:
+            database.show_base_name()
+        elif num == 7:
+            dat = view.input_2()
+            select = database.select_number(dat)
+            number = view.input_7()
+            new_user = view.input_1()
+            database.change_base(select[number - 1], new_user)
+        elif num == 8:
+            dat = view.input_2()
+            select = database.select_number(dat)
+            number = view.input_8()
+            database.delete_name_base(select[number -1])
+        elif num == 9:
+            print('Программа завершена.')
+            break
 
 
-def show_base():
-    with open('BASE.txt', 'r', encoding='utf-8') as file:
-        wi = file.read()
-        print(wi)
-        input("Нажмите клавижшу для продолжения: ")
+main()
