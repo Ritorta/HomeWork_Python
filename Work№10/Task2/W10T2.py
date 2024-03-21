fc23c068d15f0f65f661296e4e57658ace8ece99
Ritorta <Meddesu@yandex.ru>
2024-03-21 10:11:39 +0500

    add information

--- Work№10/Task2/W10T2.py
+++ Work№10/Task2/W10T2.py
@@ -59,11 +59,14 @@
 # ------------
 
 class Dragon:
+
+    # Функция инициализация класса Dragon
     def __init__(self, height, danger, color):
         self.height = height
         self.danger = danger
         self.color = color
 
+    # Функция сравнения
     def __lt__(self, other):
         if self.height != other.height:
             return self.height < other.height
@@ -71,9 +74,11 @@ class Dragon:
             return self.danger < other.danger
         return self.color < other.color
 
+    # Функция сравнения
     def __eq__(self, other):
         return self.height == other.height and self.danger == other.danger and self.color == other.color
 
+    # Функция сравнения
     def __le__(self, other):
         if self.height != other.height:
             return self.height <= other.height
@@ -81,26 +86,32 @@ class Dragon:
             return self.danger <= other.danger
         return self.color <= other.color
 
+    # Функиция сложения Драконов
     def __add__(self, other):
         new_color = min(self.color, other.color)
         new_height = (self.height + other.height) // 2
         new_danger = max(self.danger, other.danger)
         return Dragon(new_height, new_danger, new_color)
 
+    # Функция вычитания Драконов
     def __sub__(self, number):
         new_height = self.height // number
         new_danger = self.danger % number
         return Dragon(new_height, new_danger, self.color)
 
+    # Функция вызова строки
     def __call__(self, string):
         return string * self.danger
 
+    # Функция изменения цвета
     def change_color(self, new_color):
         self.color = new_color
 
+    # Функция возврата строкогого представления
     def __str__(self):
         return f"Dragon with height {self.height}, danger {self.danger} and color {self.color}."
 
+    # Функция возврата возврата объекта в виде строки
     def __repr__(self):
         return f"Dragon({self.height}, {self.danger}, {self.color})"
 
